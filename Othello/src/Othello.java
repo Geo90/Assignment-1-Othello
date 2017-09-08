7d424543d3171d02a5a1370b2bd0b6ede87b39d2
Unknown <George--Albert@hotmail.com>
2017-09-08 12:21:04 +0200

    Minor changes and updates of variablenames

--- Othello/src/Othello.java
+++ Othello/src/Othello.java
@@ -1,13 +1,17 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 import javax.swing.JFrame;
 
 public class Othello {
 	private static GameState gState;
 	private static GameEngine gEngine;
-	private static GUI gui;
-	public static void main(String[] args) {
-		  GUI play= new GUI();
-		  play.GUI();
-
-	}
+	private static GUI play;
 	
+	public static void main(String[] args) {
+		  play= new GUI();
+		  play.GUI();
+		  
+	}
 }
