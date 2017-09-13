9ef2de7e1205f138de09d87de143e8db0101f6c5
Unknown <George--Albert@hotmail.com>
2017-09-13 15:24:55 +0200

    Added methods and structure to MiniMaxAgent.

--- Othello/src/Othello/MiniMaxAgent.java
+++ Othello/src/Othello/MiniMaxAgent.java
@@ -2,4 +2,31 @@ package Othello;
 
 public class MiniMaxAgent {
 
+	GameState clonedGameState;
+	int depth = 3;
+	int utilityValue = 0;
+	int alpha = 0;
+	int beta = 0;
+	
+	public MiniMaxAgent(GameState gameState){
+		clonedGameState = gameState.cloneGameState(); 
+	}
+	
+	/**
+	 * Calculating the value of a move.
+	 * Value = 0, nothing happens,
+	 * Value = -1, opponent scores,
+	 * Value = +1, agent scores,
+	 */
+	private void utilityFunction(){
+		
+	}
+	
+	private void simulateMove(){
+		
+	}
+	
+	private void alphaBetaPruning(){
+		
+	}
 }
--- Othello/src/Othello/Othello.java
+++ Othello/src/Othello/Othello.java
@@ -11,9 +11,10 @@ class Othello {
 		System.out.println(gState.toString());
 
 		
-		 BinarySearchTree<Integer,Integer> bst = new BinarySearchTree<Integer,Integer>();
+		 /*BinarySearchTree<Integer,Integer> bst = new BinarySearchTree<Integer,Integer>();
 	        bst.put( new Integer(50), new Integer(50) );
 	        bst.put( new Integer(30), new Integer(30) );
+	        bst.put(new Integer(10), new Integer(10) );
 	        bst.put( new Integer(70), new Integer(70) );
 	        bst.put( new Integer(20), new Integer(20) );
 	        bst.put( new Integer(40), new Integer(40) );
@@ -21,15 +22,17 @@ class Othello {
 	        bst.put( new Integer(80), new Integer(80) );
 	        bst.put( new Integer(75), new Integer(75) );
 	        
+
+	        
 	        bst.root().print();
-	        bst.root().showTree();
+	        bst.root().showTree();*/
 	}
 	
 	public static void startGame(){
 		gState = new GameState();
 		gEngine = new GameEngine(); //gets the current GameState and is used to update the board every move
+		MiniMaxAgent mmAgent = new MiniMaxAgent(gState);//the algorithm agent
 		HumanAgent hAgent = new HumanAgent();//gets the current GameState and shows it to the user every move
-		MiniMaxAgent mmAgent = new MiniMaxAgent();//the algorithm agent
 	}
 	
 	public static void tick(){
